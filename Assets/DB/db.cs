6ed6536a5411d6f844ec6b4fbabba24f97ca84d1
quebeh <bullitt.awa@gmail.com>
2022-03-15 19:54:50 +0700

    post request

---
+++ Assets/DB/db.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+
+public class db : MonoBehaviour {
+    public InputField email;
+    public InputField password;
+    public InputField username;
+
+    public void req() {
+        StartCoroutine(postRequest("https://gateway.arsa.me/application/leksus/register.php", (JSONNode res) => {
+            Debug.Log(res);
+        }));
+    }
+
+    IEnumerator postRequest(string url, System.Action<JSONNode> callback) {
+        var uwr = new UnityWebRequest(url, "POST");
+
+        yield return uwr.SendWebRequest();
+
+        if (!uwr.isNetworkError) {
+            JSONNode data = JSON.Parse(uwr.downloadHandler.text);
+            callback(data);
+        }
+    }
+}
